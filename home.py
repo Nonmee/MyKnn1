9d035f42b1a71f3199d049139b3028fd506b8031
Nonmee <155877520+Nonmee@users.noreply.github.com>
2024-11-19 14:35:26 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("website Developing using Python")
-st.header("website Developing using Python😀😀")
+st.title("Website Developing using Python")
+st.header("Website Developing using Python😀😀")
 
 st.subheader("Khunanon Suittipong")
